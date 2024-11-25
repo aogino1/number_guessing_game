8d54ee40d42fcd791cddc20b57e208d0e5e5c97f
aogino1 <abdel.tmrt@gmail.com>
2024-11-25 12:28:41 +0000

    feat: complete code

--- number_guess.sh
+++ number_guess.sh
@@ -32,57 +32,57 @@ else
   done
 fi
 
-# # Start game
-# echo "Guess the secret number between 1 and 1000:"
-# NUMBER_OF_GUESSES=0
+# Start game
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
 
-# # Game loop
-# while true
-# do
-#   read GUESS
+# Game loop
+while true
+do
+  read GUESS
   
-#   # Check if input is an integer
-#   if [[ ! $GUESS =~ ^[0-9]+$ ]]
-#   then
-#     echo "That is not an integer, guess again:"
-#     continue
-#   fi
+  # Check if input is an integer
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
   
-#   # Increment guess counter
-#   ((NUMBER_OF_GUESSES++))
+  # Increment guess counter
+  ((NUMBER_OF_GUESSES++))
   
-#   # Compare guess with secret number
-#   if [[ $GUESS -eq $SECRET_NUMBER ]]
-#   then
-#     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+  # Compare guess with secret number
+  if [[ $GUESS -eq $SECRET_NUMBER ]]
+  then
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
     
-#     # Update user stats
-#     if [[ -z $USER_INFO ]]
-#     then
-#       # First game for new user
-#       UPDATE_STATS=$($PSQL "UPDATE users SET games_played = 1, best_game = $NUMBER_OF_GUESSES WHERE username = '$USERNAME'")
-#     else
-#       # Update existing user stats
-#       echo "$USER_INFO" | while IFS="|" read GAMES_PLAYED BEST_GAME
-#       do
-#         # Calculate new best game
-#         if [[ $BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
-#         then
-#           BEST_GAME=$NUMBER_OF_GUESSES
-#         fi
+    # Update user stats
+    if [[ -z $USER_INFO ]]
+    then
+      # First game for new user
+      UPDATE_STATS=$($PSQL "UPDATE users SET games_played = 1, best_game = $NUMBER_OF_GUESSES WHERE username = '$USERNAME'")
+    else
+      # Update existing user stats
+      echo "$USER_INFO" | while IFS="|" read GAMES_PLAYED BEST_GAME
+      do
+        # Calculate new best game
+        if [[ $BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
+        then
+          BEST_GAME=$NUMBER_OF_GUESSES
+        fi
         
-#         # Increment games played
-#         ((GAMES_PLAYED++))
+        # Increment games played
+        ((GAMES_PLAYED++))
         
-#         # Update database
-#         UPDATE_STATS=$($PSQL "UPDATE users SET games_played = $GAMES_PLAYED, best_game = $BEST_GAME WHERE username = '$USERNAME'")
-#       done
-#     fi
-#     break
-#   elif [[ $GUESS -gt $SECRET_NUMBER ]]
-#   then
-#     echo "It's lower than that, guess again:"
-#   else
-#     echo "It's higher than that, guess again:"
-#   fi
-# done
+        # Update database
+        UPDATE_STATS=$($PSQL "UPDATE users SET games_played = $GAMES_PLAYED, best_game = $BEST_GAME WHERE username = '$USERNAME'")
+      done
+    fi
+    break
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
